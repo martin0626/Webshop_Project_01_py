4db0cf32f67c839b2ceb649c8bde0220c5e72a40
Martin Ivanov <scbsteel12@gmail.com>
2022-04-29 22:54:48 +0300

    Delete Webshop_01 directory

--- Webshop_01/Webshop_01/shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from Webshop_01.shop.views import test_view
-
-urlpatterns = [
-    path('', test_view, name='test'),
-]
--- Webshop_01/Webshop_01/shop/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-import django.views.generic as class_views
-
-
-def test_view(request):
-    return render(request, 'asd.html')
